11a4f20b189cd916892ce318df6d16cb52fc9cf7
Vinicius Vasconcelos <vasconcelosvcd@gmail.com>
2019-11-21 16:49:17 -0300

    fix enviroment url

--- vars.go
+++ vars.go
@@ -3,12 +3,12 @@ package cielo
 var (
 	// ProductionEnvironment sets the environment to production
 	ProductionEnvironment = Environment{
-		APIUrl:      "https://api.cieloecommerce.cielo.com.br/",
-		APIQueryURL: "https://apiquery.cieloecommerce.cielo.com.br/",
+		APIUrl:      "https://api.cieloecommerce.cielo.com.br",
+		APIQueryURL: "https://apiquery.cieloecommerce.cielo.com.br",
 	}
 	// SandboxEnvironment sets the environment to sandbox
 	SandboxEnvironment = Environment{
-		APIUrl:      "https://apisandbox.cieloecommerce.cielo.com.br/",
-		APIQueryURL: "https://apiquerysandbox.cieloecommerce.cielo.com.br/",
+		APIUrl:      "https://apisandbox.cieloecommerce.cielo.com.br",
+		APIQueryURL: "https://apiquerysandbox.cieloecommerce.cielo.com.br",
 	}
 )
