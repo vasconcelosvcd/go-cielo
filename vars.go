7694a4ac5e18c67c5efd631d88fae6a4b60c137b
Walter Junior <walter.junior@endurance.com>
2020-11-23 17:44:09 -0300

    Adjust with env variables

--- vars.go
+++ vars.go
@@ -1,14 +1,16 @@
 package cielo
 
+import "os"
+
 var (
 	// ProductionEnvironment sets the environment to production
 	ProductionEnvironment = Environment{
-		APIUrl:      "https://api.cieloecommerce.cielo.com.br",
-		APIQueryURL: "https://apiquery.cieloecommerce.cielo.com.br",
+		APIUrl:      os.Getenv("PRODUCTION_API_URL"),
+		APIQueryURL: os.Getenv("PRODUCTION_API_QUERY_URL"),
 	}
 	// SandboxEnvironment sets the environment to sandbox
 	SandboxEnvironment = Environment{
-		APIUrl:      "https://apisandbox.cieloecommerce.cielo.com.br",
-		APIQueryURL: "https://apiquerysandbox.cieloecommerce.cielo.com.br",
+		APIUrl:      os.Getenv("SANDBOX_API_URL"),
+		APIQueryURL: os.Getenv("SANDBOX_API_QUERY_URL"),
 	}
 )
